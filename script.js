b09d7512d1718f5027b7fcfdc8e37e63bc72e6b5
Manjiri Pund <113796944+Manjiripund@users.noreply.github.com>
2025-12-04 11:41:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let currentAudio=null;
+const buttons=document.querySelectorAll(".btn");
+
+buttons.forEach(btn=>{
+	btn.addEventListner("click",()=>{
+		if(btn.classList.contains("stop")){
+			if(currentAudio){
+				currentAudio.pause();
+
+				currentAudio.currentTime=0;
+			}
+			return;
+		}
+		if(currentAudio){
+			currentAudio.pause();
+			currentAudio.currentTime=0;
+		}
+		let soundName= btn.getAtrribute("data-sound");
+		currentAudio=new Audio("sound/" + soundName + ".mp3");
+		currentAudio.play();
+	});
+});
